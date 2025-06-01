9833a5dbc6b6b20149a380b2acfc1a9c1445935e
luimo-ch <starry.make.0t@icloud.com>
2025-06-01 13:28:29 +0200

    initial commit

---
+++ src/main/java/ch/luimo/flashsale/flashsaleeventsapi/FlashsaleEventsApiApplication.java
@@ -0,0 +1,13 @@
+package ch.luimo.flashsale.flashsaleeventsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashsaleEventsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlashsaleEventsApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ch/luimo/flashsale/flashsaleeventsapi/FlashsaleEventsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.luimo.flashsale.flashsaleeventsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashsaleEventsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
